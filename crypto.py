936e1ac89da6ba9a409ec9580eab5f9cc7ad47ef
Kirza <kirzaru@gmail.com>
2018-06-06 08:10:51 +0300

    Final v2.0

--- crypto.py
+++ crypto.py
@@ -1,43 +1,14 @@
 import random, math
 
-STARTING_NUMBER=571828182881
-
-def fast_module_pow(base, degree, module):
-    degree = bin(degree)[2:]
-    r = 1
-    for i in range(len(degree) - 1, -1, -1):
-        r = (r * base ** int(degree[i])) % module
-        base = (base ** 2) % module
-    return r
+STARTING_NUMBER = 571828182881
 
 def pow2(base, degree, module):
     result = base
-    # i = 0
     bin_degree = "{:b}".format(degree)
     bin_degree = bin_degree[1:]
     for bit in bin_degree:
-        # if i == 0:
-            # print(bit, "| a", i, "=", result, "mod", module, "| iteration ##", i)
-            # i = i + 1
-            # continue
-        # else:
         result = ((result ** 2) * (base ** int(bit))) % module
-            # if bit == "0":
-            #     result = (result ** 2) % module
-            # else:
-            #     result = ((result ** 2) * base) % module
-            # print(bit, "| a", i, "=", result, "mod", module, "| iteration #", i)
-            # i = i + 1
     return result
-
-
-# def gcd(first_num, second_num):
-#     while first_num != second_num:
-#         if first_num > second_num:
-#             first_num = first_num - second_num
-#         else:
-#             second_num = second_num - first_num        
-#     return first_num 
 
 def gcd(first_num, second_num):
   while second_num:
@@ -45,7 +16,7 @@ def gcd(first_num, second_num):
   return first_num
 
 def maurer_number(parent_number):
-    maurer_modifier = random.randint((parent_number + 1)/2, (4 * parent_number + 2)/2) * 2
+    maurer_modifier = random.randint((parent_number + 1) / 2, (4 * parent_number + 2) / 2) * 2
     new_prime = parent_number * maurer_modifier + 1
     return [int(new_prime), int(maurer_modifier)]
 
@@ -58,58 +29,45 @@ def pow_basic_presentation(new_prime):
     return [int(inner_odd_number), int(two_degree)]
 
 def hundred_primes_test(new_prime, prime_tab):
-    flag = 0
+    flag = 0 # test passed
     if (new_prime > 100):
         for i in range (25):
             if (new_prime % prime_tab[i] == 0):
-                flag = 1
+                flag = 1 # test failed
                 break
     return flag
 
-def rabin_miller_v2(inner_odd_number, new_prime, two_degree):
-    random_test_number = random.randint(1, new_prime - 1)
-    # print('Selected a number =', random_test_number)
-    if (gcd(new_prime, random_test_number) != 1):
-        # print("Checked (", random_test_number, ",", new_prime, ") != 1. Result = True. a - good number, new_prime - composite")
-        return 1
+def rabin_miller(inner_odd_number, new_prime, two_degree):
+    test_number = random.randint(1, new_prime - 1)
+    if (gcd(new_prime, test_number) != 1):
+        return 1 # test_number is "good"
     else:
-        if (pow2(random_test_number, inner_odd_number, new_prime) == 1):
-            return 0
+        if (pow2(test_number, inner_odd_number, new_prime) == 1):
+            return 0 # test_number is "bad"
         for i in range(two_degree - 1):
-            if (pow2(random_test_number, pow(2, i) * inner_odd_number, new_prime) == -1):
-                return 0
-        return 1
+            if (pow2(test_number, pow(2, i) * inner_odd_number, new_prime) == -1):
+                return 0 # test_number is "bad"
+        return 1 # test_number is "good"
 
-def mauer_final_test(new_prime, maurer_modifier):
+def maurer_final_test(new_prime, maurer_modifier):
     for i in range(30):
         if i == 0:
-            random_test_number = 2
+            test_number = 2
         else:
-            random_test_number = random.randint(2, new_prime - 2)
-        # print(i, "(in maurer_final_test)")
-        print('#' * 50)
-        print('args:', random_test_number, new_prime - 1, new_prime)
-        print('2args:', random_test_number, maurer_modifier, new_prime)
-        if ((pow2(random_test_number, new_prime - 1, new_prime) == 1) and (gcd(pow(random_test_number, maurer_modifier)-1, new_prime) == 1)):
-        # x = pow(random_test_number, maurer_modifier, new_prime)-1
-        # print("I am in maurer_final_test and going to count if gcd(", x, "," , new_prime,")==1")
-        #if (gcd(pow(random_test_number, maurer_modifier, new_prime)-1, new_prime) == 1):
-            # print("(TRUE in maurer_final_test on iteration #)", i)
-            return True
-        # else:
-            # print("meh #", i)
-    # print("(False in maurer_final_test)")
-    return False
+            test_number = random.randint(2, new_prime - 2)
+        if ((pow2(test_number, new_prime - 1, new_prime) == 1) and (gcd(pow(test_number, maurer_modifier)-1, new_prime) == 1)):
+            return True # test passed
+    return False # test failed
 
-# Main
-
-parent_number = int(input('Enter basic number '))
-random.seed()
+# Main:values
 numbers_checked = 0
 prime_tab = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-prime_tab_counter = 0
+hundred_primes_test_fail = 0
 rabin_fail_counter = 0
-mauer_final_test_fail_counter = 0
+maurer_final_test_fail_counter = 0
+# Main:algo
+parent_number = int(input('Enter prime number '))
+random.seed()
 while True:
     new = maurer_number(parent_number)
     new_prime = new[0]
@@ -118,37 +76,24 @@ while True:
     basic_representation = pow_basic_presentation(new_prime)
     two_degree = basic_representation[1]
     inner_odd_number = basic_representation[0]
-    flag = hundred_primes_test(new_prime, prime_tab)
-    if (flag == 1):
-        prime_tab_counter += 1
+    if (hundred_primes_test(new_prime, prime_tab) == 1):
+        hundred_primes_test_fail += 1
         continue
     rabin_test_result = 0
     for _ in range(5):
-        rabin_test_result += rabin_miller_v2(inner_odd_number, new_prime, two_degree)
+        rabin_test_result += rabin_miller(inner_odd_number, new_prime, two_degree)
         if (rabin_test_result != 0):
             break
     if (rabin_test_result != 0):
         rabin_fail_counter += 1
-    elif (mauer_final_test(new_prime, maurer_modifier) == True):
+        continue
+    elif (maurer_final_test(new_prime, maurer_modifier) == True):
         break
     else:
-        mauer_final_test_fail_counter += 1
-        
+        maurer_final_test_fail_counter += 1
+# Main:output   
 print("Generated prime number:", new_prime)
-print("To find it I checked", numbers_checked, "numbers, created with maurer algorithm")
-print(prime_tab_counter, " of them failed on hundred prime numbers division test")
-print(rabin_fail_counter, " of them failed on rabin-miller test")
-print(mauer_final_test_fail_counter, " of them failed on mauer_final_test_fail_counter")
-
-# POW2TEST
-# random.seed()
-# flag = True
-# for _ in range(50):
-#     a = random.randint(2, 100000000)
-#     b = random.randint(2, 100000000)
-#     c = random.randint(2, 100000000)
-#     if pow(a, b, c) != pow2(a, b, c):
-#         flag = False
-# print(flag)
-# # print(pow(a, b, c), pow2(a, b, c))
-# print("for a =", a, "b =", b, "c =", c, "|", "pow =", pow(a, b, c), "| pow2 = ", pow2(a, b, c))
+print("To find it I checked", numbers_checked, "numbers, created with Maurer algorithm")
+print(hundred_primes_test_fail, " of them failed on hundred primes division test")
+print(rabin_fail_counter, " of them failed on Rabin-Miller test")
+print(maurer_final_test_fail_counter, " of them failed on Maurer final test")
